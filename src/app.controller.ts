2ba6ea9cfed8487604299705515016a377ed7543
Dom Nguyen <nguyendaithang23061997@gmail.com>
2025-12-04 00:15:43 +0700

    feat(update): update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/health')
+  health(): string {
+    // check DB + Redis nếu cần
+    return 'OK';
+  }
 }
