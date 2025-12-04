7efe987e4bfbe497387408bf891a51a5cff13135
Dom Nguyen <nguyendaithang23061997@gmail.com>
2025-12-04 11:09:20 +0700

    feat(update): update docker entrypoint file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,12 @@ while ! nc -z $DB_HOST $DB_PORT; do
 done
 echo "Database started"
 
+echo "Waiting for Redis..."
+while ! nc -z ${REDIS_HOST:-redis} ${REDIS_PORT:-6379}; do
+  sleep 0.1
+done
+echo "Redis started"
+
 echo "Running Prisma migrations..."
 npx prisma migrate deploy
 npx prisma generate
