eca50d05954128bc1ed0827085abc0623847f3be
Dom Nguyen <nguyendaithang23061997@gmail.com>
2026-04-12 21:59:09 +0700

    feat: implement gateway module with proxy services and configure project environment settings

--- src/gateway/proxies/booking.proxy.ts
+++ src/gateway/proxies/booking.proxy.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class BookingProxyService {
@@ -9,21 +10,48 @@ export class BookingProxyService {
   constructor(private http: HttpService) {}
 
   async createBooking(payload: any) {
-    const res = await firstValueFrom(
-      this.http.post<any>(`${this.baseUrl}/`, payload),
-    );
-    return res.data;
+    try {
+      const res = await firstValueFrom(
+        this.http.post<any>(`${this.baseUrl}/`, payload),
+      );
+      return res.data;
+    } catch (error) {
+      this.handleError(error, 'Error creating booking');
+    }
   }
 
   async getAllBookings() {
-    const res = await firstValueFrom(this.http.get<any>(`${this.baseUrl}/`));
-    return res.data;
+    try {
+      const res = await firstValueFrom(this.http.get<any>(`${this.baseUrl}/`));
+      return res.data;
+    } catch (error) {
+      this.handleError(error, 'Error fetching all bookings');
+    }
   }
 
   async getBookingById(id: string) {
-    const res = await firstValueFrom(
-      this.http.get<any>(`${this.baseUrl}/${id}`),
-    );
-    return res.data;
+    try {
+      const res = await firstValueFrom(
+        this.http.get<any>(`${this.baseUrl}/${id}`),
+      );
+      return res.data;
+    } catch (error) {
+      this.handleError(error, `Error fetching booking with ID ${id}`);
+    }
+  }
+
+  private handleError(error: any, message: string) {
+    if (error instanceof AxiosError) {
+      const status = error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR;
+      const data = error.response?.data || { message: error.message };
+      throw new HttpException(
+        {
+          message,
+          gatewayError: data,
+        },
+        status,
+      );
+    }
+    throw new HttpException(message, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
